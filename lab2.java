246d95ce6a898a0f6c2cbba436828000504cbde8
gotoVetka <noned4b@gmail.com>
2025-10-16 21:03:23 +0300

    Lab2  ended

--- lab2.java
+++ lab2.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class lab2 {
     public static void main(String[] args){
         Tasks.task1();
-
+        Tasks.task2();
     }
 }
 public class Tasks{
@@ -25,7 +25,42 @@ public class Tasks{
         }
     }
 
-    public void task2(){
+    public static void task2(){
+        int rows, columns, currentLength = 1, maxLength = 1;
+        System.out.println("Enter number of rows: ");
+        rows = scan.nextInt();
+        System.out.println("Enter number of columns: ");
+        columns = scan.nextInt();
+        if(rows*columns==0){
+            System.out.println("Matrix size is ass");
+            return;
+        }
+        int[] matrix = new int[rows*columns];
+        for(int i = 0; i < rows*columns; i++){
+            System.out.print(": ");
+            matrix[i] = scan.nextInt(); 
+        }
+        printArray(matrix, rows, columns);
 
+        for (int i = 1; i < rows*columns; i++) {
+            if (matrix[i] > matrix[i - 1]) {
+                currentLength++;
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                }
+            } else {
+                currentLength = 1;
+            }
+        }
+        System.out.println("number > in a row: " + maxLength);
+    }
+
+    public static void printArray(int[] arr, int rows, int columns){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print(arr[i*columns+j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
